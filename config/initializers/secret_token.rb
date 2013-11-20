91340e5857371f593cbf920a19e10dae298bad4e
Daniel Stutzman <dtstutz@gmail.com>
2013-11-20 09:52:19 -0700

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HomeworkTracker::Application.config.secret_key_base = 'b4a0cd29eb6432f041bb7a2bdcada8f503be49fb4c4a1fe2a306aeb6f739b49f3fc071dbdb38fad42b65e49c2645dda636aad4b2c9ad2f9b044690289b9a91dc'
